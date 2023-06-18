83eb92a3c972c5c05ce17f05810ffaffa1d61cdd
DESMOND <63297674+A-Desmond@users.noreply.github.com>
2023-06-18 17:58:38 +0000

    Initial Commit

---
+++ todo mvvm app/todo_mvvm_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_mvvm_appApp.swift
+//  todo mvvm app
+//
+//  Created by desmond on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_mvvm_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
